6cde7be186360dc4d3cfcc14f4d33deacf106df2
 <Mac@s-MacBook-Pro.local>
2019-07-25 10:18:04 -0400

    created knexfile and configured connection

--- api/server.js
+++ api/server.js
@@ -3,8 +3,8 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 
-// const authRouter = require('../auth/auth-router.js');
-// const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
@@ -12,8 +12,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use('/api/auth', authRouter);
-// server.use('api/users', userRouter);
+server.use('/api/auth', authRouter);
+server.use('api/users', usersRouter);
 
 server.get('/', (req, res) => {
     res.send("It's Working");
